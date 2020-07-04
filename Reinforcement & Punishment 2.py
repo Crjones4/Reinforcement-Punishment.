7489328f1f6003655a9dbe3d94f94b5bbdb89103
Crjones4 <66502028+Crjones4@users.noreply.github.com>
2020-07-04 12:19:31 -0400

    Add files via upload

---
+++ Reinforcement & Punishment 2.py
@@ -0,0 +1,4 @@
+def punishment ():
+    for x in range (0,101):
+        print("I will never spam my friends again.")
+punishment()
---
+++ Reinforcement and Punishment.py
@@ -0,0 +1,8 @@
+def is_multiple (n,m):
+    if n%m ==0:
+        print ("True")
+
+    else: 
+        print ("False")
+    return
+
